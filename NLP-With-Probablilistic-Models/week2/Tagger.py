c6dd00d4d5a23a2e98d4ef12cb315565f12c7fe5
Jeremy Miller <jeremy.miller@clarivate>
2021-01-24 16:02:17 -0800

    change tansition matrix to np array

--- NLP-With-Probablilistic-Models/week2/Tagger.py
+++ NLP-With-Probablilistic-Models/week2/Tagger.py
@@ -16,10 +16,13 @@ class Tagger():
         self.d_matrix = None
         self.tag_counts = None
         self.tag_pair_counts = None
+        self.ordered_tag_pair_set = None
+        self.ordered_tag_set = None
 
     def populate_transition_matrix(self, data, epsilon=0.0001):
         """
         Load the brown tagged sentence corpus from NLTK
+        Define 'INIT' as the start of a sentence tag, transition to first tag
         :param data: nltk brown tagged sentence corpus, or similar
         :param epsilon: regularization; avoids p==1 and div by 0
         :return: None; populates transition matrix attribute
@@ -27,20 +30,34 @@ class Tagger():
         tag_pairs = []
         tag_counts = []
         for sent in data:
+            tag_pairs.append(("INIT", sent[0][1]))
+            tag_counts.append("INIT")
             for i in range(len(sent) - 1):
                 tag1 = sent[i][1]
                 tag2 = sent[i + 1][1]
                 tag_pairs.append((tag1, tag2))
                 tag_counts.append(tag1)
-        N = len(set(tag_counts))
+        self.ordered_tag_set = sorted(set(tag_counts))
+        N = len(self.ordered_tag_set)
+
         self.tag_counts = Counter(tag_counts)
         self.tag_pair_counts = Counter(tag_pairs)
-        self.transition_matrix = {}
+        self.ordered_tag_pair_set = sorted(self.tag_pair_counts.keys())
+        # self.transition_matrix = {}
+        self.transition_matrix = np.zeros(
+            (len(self.ordered_tag_set), len(self.ordered_tag_set)),
+            dtype="float32")
 
-        for tag_pair in self.tag_pair_counts.keys():
-            numerator = self.tag_pair_counts.get(tag_pair) + epsilon
-            denominator = self.tag_counts.get(tag_pair[0]) + (N*epsilon)
-            self.transition_matrix[tag_pair] = numerator / denominator
+        for i in range(len(self.ordered_tag_set)):
+            for j in range(len(self.ordered_tag_set)):
+                pair = (self.ordered_tag_set[i], self.ordered_tag_set[j])
+                try:
+                    n = self.tag_pair_counts.get(pair) + epsilon
+                    d = self.tag_counts.get(self.ordered_tag_set[i]) + (N*epsilon)
+                    print(pair, n, d)
+                    self.transition_matrix[i][j] = n / d
+                except:
+                    continue
 
     def populate_emission_matrix(self, data):
         # get all state_word counter
@@ -49,7 +66,6 @@ class Tagger():
             lowered = [(word[1], word[0].lower()) for word in sent] # flip to Tag, String
             c.update(lowered)
         self.emission_matrix = c
-
 
     def viterbi_init(self):
         pass
